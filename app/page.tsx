f142747cf9f56211cff5e19ef6ab3cbfd6edf14e
Enzo Bozzani <bozzanienzo@gmail.com>
2024-08-22 16:32:34 -0300

    refactor: refactoring hero section using cds grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,33 @@
+'use client';
+
+import { Theme, Content } from '@carbon/react';
 import { ArrowDown } from '@carbon/icons-react';
 
 import { MainHeader } from '@/components/MainHeader';
-import { LectureService } from '@/services/LectureService';
 import { HeroSection } from '@/components/HeroSection';
 import { SpeakersSection } from '@/components/SpeakersSection';
-import { SpeakerService } from '@/services/SpeakerService';
 import { SpeakerModal } from '@/components/SpeakerModal';
+import { speakers } from '@/constants/speakers';
 
 import styles from './page.module.scss';
 
-const HomePage = async () => {
-	const speakersPromise = SpeakerService.getAll();
-	const lecturesPromise = LectureService.getAll();
-
-	const [speakers, lecture] = await Promise.all([speakersPromise, lecturesPromise]);
-
+const HomePage = () => {
 	return (
-		<main className={styles.main}>
-			<MainHeader />
-			<div className={styles.pageContent}>
-				<HeroSection />
-				<div className={styles.arrowRow}>
-					<a href='#speakers'>
-						<ArrowDown />
-					</a>
-				</div>
-				<SpeakersSection speakers={speakers} />
-			</div>
-			<SpeakerModal />
-		</main>
+		<Theme theme='white'>
+			<main className={styles.main}>
+				<MainHeader />
+				<Content className={styles.content}>
+					<HeroSection />
+					<div className={styles.arrowRow}>
+						<a href='#speakers'>
+							<ArrowDown />
+						</a>
+					</div>
+					<SpeakersSection speakers={speakers} />
+				</Content>
+				<SpeakerModal />
+			</main>
+		</Theme>
 	);
 };
 
