e1d071f0c8254a72fe1d9ce0a35358d44e2363bf
Enzo Bozzani <enzobozzani@ibm.com>
2024-08-23 14:55:08 -0300

    feat hero section

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -12,44 +12,43 @@ export const HeroSection = () => {
 	return (
 		<section className={cn('cds--grid', styles.section)}>
 			<div className='cds--row'>
-				<div className={cn('cds--col-sm-2 cds--col-md-4 cds--col-lg-8', styles.titleSide)}>
+				<div className={cn('cds--col-sm-4 cds--col-md-4 cds--col-lg-8', styles.titleSide)}>
 					<div className='cds--grid'>
-						<h3 className='cds--row'>
+						<h3 className={cn('cds--row', styles.ibmTitle)}>
 							IBM <strong>Research</strong> Brasil
 						</h3>
-						<h1 className='cds--row'>
+						<h1 className={cn('cds--row', styles.mainTitle)}>
 							Colóquio <strong>2023</strong>
 						</h1>
-						<h3 className='cds--row'>
-							Tecnologias <span className='highlight'>responsáveis e inclusivas</span>
+						<h3 className={cn('cds--row', styles.secondaryTitle)}>
+							Tecnologias <span>responsáveis e inclusivas</span>
 						</h3>
-						<div className='cds--row cds--grid'>
-							<div className='cds--col'>
-								<div className='cds--row'>
+						<div className={cn(styles.info)}>
+							<div>
+								<div className={styles.iconRow}>
 									<Time className={styles.icon} />
 									<span>09:00 - 18:00</span>
 								</div>
-								<div className='cds--row'>
+								<div className={styles.iconRow}>
 									<Calendar className={styles.icon} />
 									<span>16 de maio, evento presencial</span>
 								</div>
-								<div className='cds--row'>
+								<div className={styles.iconRow}>
 									<Location className={styles.icon} />
 									<span>Rua Tutóia, 1157 - São Paulo</span>
 								</div>
 							</div>
-							<div className='cds--col'>
-								<Button className={styles.subscribeBtn}>
+							<div className={cn(styles.subscribeBtnDiv)}>
+								<Button renderIcon={ArrowRight}>
 									<Link href={'/'}>
 										<span>Inscreva-se</span>
-										<ArrowRight />
 									</Link>
 								</Button>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div className={cn('cds--col-sm-2 cds--col-md-4 cds--col-lg-8', styles.imgSide)}>
+				<div className={cn('cds--col-sm-0 cds--col-md-4 cds--col-lg-8', styles.imgSide)}>
 					<div>
 						<Image
 							alt='hero section'
