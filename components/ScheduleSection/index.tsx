7d6975ee6596f83c66fdde0d549d813138243df3
Enzo Bozzani <bozzanienzo@gmail.com>
2024-08-27 11:09:48 -0300

    feat: init schedule section

--- components/ScheduleSection/index.tsx
+++ components/ScheduleSection/index.tsx
@@ -1,4 +1,8 @@
+import { Theme } from '@carbon/react';
+
 import { FormattedWindow } from '@/types';
+import { cn } from '@/utils';
+
 import styles from './styles.module.scss';
 
 export interface ScheduleSectionProps {
@@ -7,11 +11,34 @@ export interface ScheduleSectionProps {
 
 export const ScheduleSection = ({ windows }: ScheduleSectionProps) => {
 	return (
-		<section
-			id='schedule'
-			className={styles.section}
-		>
-			{JSON.stringify(windows)}
-		</section>
+		<Theme theme='g10'>
+			<section
+				id='schedule'
+				className={cn(styles.section, 'cds--grid')}
+			>
+				<div className='cds--row'>
+					<div
+						className={cn(
+							styles.titleSide,
+							'cds--offset-md-1 cds--offset-lg-1 cds--col-md-6 cds--col-lg-5 cds--col-sm-4'
+						)}
+					>
+						<h3 className={styles.title}>Agenda</h3>
+					</div>
+					<div
+						className={cn(
+							styles.windowsSide,
+							'cds--offset-md-1 cds--offset-lg-0 cds--col-lg-9 cds--col-sm-4 cds--col-md-6'
+						)}
+					>
+						{windows.map((window) => (
+							<div key={window.title}>
+								{window.startTime} + {window.title}
+							</div>
+						))}
+					</div>
+				</div>
+			</section>
+		</Theme>
 	);
 };
